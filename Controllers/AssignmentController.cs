377e3d310441e968114a620c567f61fbfe2ec9f2
Lynnerv <aythor_briceno@usmp.pe>
2025-04-27 03:33:46 -0500

    codigo Assignment (step2)

---
+++ Controllers/AssignmentController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SeleccionFutbol.Data;
+using SeleccionFutbol.Models;
+
+namespace SeleccionFutbol.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AssignmentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var asignaciones = _context.Assignments
+                .Include(a => a.Player)
+                .Include(a => a.Team)
+                .ToList();
+            return View(asignaciones);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Players = _context.Players.ToList();
+            ViewBag.Teams = _context.Teams.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Assignment assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Assignments.Add(assignment);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Players = _context.Players.ToList();
+            ViewBag.Teams = _context.Teams.ToList();
+            return View(assignment);
+        }
+    }
+}
+
