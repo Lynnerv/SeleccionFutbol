d6f6f0345b83a107626c987f4b737bdec9c9d776
Lynnerv <aythor_briceno@usmp.pe>
2025-04-27 02:51:04 -0500

    codigo inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SeleccionFutbol.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
